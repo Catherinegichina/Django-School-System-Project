7992750bab290ce985ae241afc49e7797f558479
Angelaacholdeng <dengacholangela@gmail.com>
2021-07-14 20:46:25 +0300

    schoolsystem

---
+++ schoolsystem/student/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+# Create your models here.
+class Student(models.Model):
+    first_name=models.CharField(max_length=12)
+    last_name=models.CharField(max_length=20)
+    age=models.PositiveSmallIntegerField()
+    date_of_birth=models.DateField()
